0b1c5c70f036e9d7a86b50b20b70fb0c685bdd5c
mluksic <luksic.miha@gmail.com>
2019-05-18 20:53:22 +0200

    Set env variables for database

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,13 +1,18 @@
 import { join } from 'path';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+
+const environment = process.env.NODE_ENV || 'development';
+const data: any = dotenv.parse(fs.readFileSync(`.env`));
 
 export const withCache: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: process.env.DB_HOST,
+    host: data.DB_HOST,
     port: 5432,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DB,
+    username: data.DB_USER,
+    password: data.DB_PASSWORD,
+    database: data.DB_NAME,
     synchronize: true,
     entities: [join(__dirname, './**/*.entity{.ts,.js}')],
 };
