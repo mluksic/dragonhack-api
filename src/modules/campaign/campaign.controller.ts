99055018a538c4839451e4d6e64e707eeed0ae02
mluksic <luksic.miha@gmail.com>
2019-05-19 04:12:51 +0200

    Change controllers

--- src/modules/campaign/campaign.controller.ts
+++ src/modules/campaign/campaign.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { Crud } from '@nestjsx/crud';
 import { Campaign } from './campaign.entity';
 import { CampaignService } from './campaign.service';
@@ -23,5 +23,10 @@ export class CampaignController {
     @Get()
     findAll(): Promise<CampaignDTO[]> {
         return this.service.findAll();
+    }
+
+    @Post()
+    createFolder(@Body() campaign: CampaignDTO): Promise<CampaignDTO> {
+        return this.service.createCampaign(campaign);
     }
 }
--- src/modules/campaign/campaign.service.ts
+++ src/modules/campaign/campaign.service.ts
@@ -3,6 +3,7 @@ import { Campaign } from './campaign.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RepositoryService } from '@nestjsx/crud/typeorm';
 import { Repository } from 'typeorm';
+import { CampaignDTO } from './campaign.dto';
 
 @Injectable()
 export class CampaignService extends RepositoryService<Campaign> {
@@ -16,5 +17,9 @@ export class CampaignService extends RepositoryService<Campaign> {
 
     async findAll(): Promise<Campaign[]> {
         return await this.campaignRepository.find();
+    }
+
+    async createCampaign(campaign: CampaignDTO): Promise<Campaign> {
+        return await this.campaignRepository.save(campaign);
     }
 }
